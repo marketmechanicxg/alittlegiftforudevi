0bd1b3934f192035ee776388d7feaa713a360d2e
Grizzy <grizzyxixi@email.com>
2026-06-04 13:12:35 +0700

    update

--- js/audio-system.js
+++ js/audio-system.js
@@ -14,8 +14,8 @@
 const playlist = [
   {
     id:        'song1',
-    title:     'Count On Me',
-    artist:    'Bruno Mars',
+    title:     'Shape Of My Heart',
+    artist:    'Backstreet Boys',
     file:      'music/song1.mp3',
     cover:     'assets/music-cover/song1.jpeg',
     startTime: 0,       // seconds — where playback begins
